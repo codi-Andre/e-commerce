d8d2ae2c04538db27170e9d120b7d44465c5c923
André Miranda <aegis.andre@gmail.com>
2023-10-20 20:28:27 -0300

    build: add tailwind and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,11 @@ module.exports = {
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
-    "plugin:react/jsx-runtime"
+    "plugin:react/jsx-runtime",
+    "prettier"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
-    tsconfigRootDir: __dirname
-  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"]
+}
+
+module.exports = config
